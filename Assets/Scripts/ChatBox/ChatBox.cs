2d718e3a23b9c727e96f0536ce55ceb39c8a6d28
Rafael Bezerra <rafaelbezerra195@gmail.com>
2018-06-10 21:03:58 -0300

    Development of the Dialoge System

---
+++ Assets/Scripts/ChatBox/ChatBox.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatBox : MonoBehaviour {
+
+    public Chat ChatData { get; set; }
+
+    [SerializeField]
+    private Text _characterTitle;
+    [SerializeField]
+    private Text _chat;
+    [SerializeField]
+    private Text _statusChat;
+    private Queue _chatSequence;
+
+    private const string _statusChatMenssege = "Continue";
+
+    void Start () {
+        _chatSequence = new Queue();
+    }
+	
+	void Update () {        
+	}
+
+    public void StartChat()
+    {
+        EnqueueChat();
+        NextSetence();
+    }
+
+    public void ResetChat()
+    {
+        _chatSequence.Clear();
+        _characterTitle.text = "";
+        _chat.text = "";
+        _statusChat.text = "";
+    }
+
+    public void NextSetence()
+    {
+        string[] setence = _chatSequence.Dequeue() as string[];
+
+        _characterTitle.text = setence[0];
+        _chat.text = setence[1];
+        _statusChat.text = _statusChatMenssege;
+    }
+
+    public bool HasSetences()
+    {
+        return _chatSequence.ToArray().Length > 0;
+    }
+
+    private void EnqueueChat()
+    {
+        int maxCountSetences = 0;
+        int indexSetence = 0;
+
+        _chatSequence.Clear();
+
+        for(int i = 0; i < ChatData.chat.Length; i++)
+        {
+            maxCountSetences = ChatData.chat[i].senteces.Length > maxCountSetences ? ChatData.chat[i].senteces.Length : maxCountSetences;
+        }
+
+        while( indexSetence < maxCountSetences)
+        {
+            for (int i = 0; i < ChatData.chat.Length; i++)
+            {
+                if( indexSetence < ChatData.chat[i].senteces.Length)
+                {
+                    _chatSequence.Enqueue(new string[2] { ChatData.chat[i].charName, ChatData.chat[i].senteces[indexSetence] });
+                }                
+            }
+
+            indexSetence++;
+        }
+    }   
+}
---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public Chat chat;
+    private UIManager _uiManager;    
+
+    private void Start()
+    {        
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+    }
+
+    private void OnTriggerEnter2D()
+    {
+        _uiManager.SetChat(chat);
+    }
+
+    private void OnTriggerExit2D()
+    {
+        _uiManager.SetChat(null);
+    }
+}
